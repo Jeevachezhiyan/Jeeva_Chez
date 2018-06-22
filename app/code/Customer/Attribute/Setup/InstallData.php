f539d1a3595133a31d952180fac608fe481595e7
Jeevachezhiyan <jeevachezhiyan@gmail.com>
2018-06-22 18:03:33 +0530

    Customer Attribute

---
+++ app/code/Customer/Attribute/Setup/InstallData.php
@@ -0,0 +1,82 @@
+<?php
+
+
+namespace Customer\Attribute\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Customer\Model\Customer;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+
+    private $customerSetupFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Customer\Setup\CustomerSetupFactory $customerSetupFactory
+     */
+    public function __construct(
+        CustomerSetupFactory $customerSetupFactory
+    ) {
+        $this->customerSetupFactory = $customerSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install(
+        ModuleDataSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+
+        $customerSetup->addAttribute(\Magento\Customer\Model\Customer::ENTITY, 'website', [
+            'type' => 'varchar',
+            'label' => 'Website',
+            'input' => 'text',
+            'source' => '',
+            'required' => false,
+            'visible' => true,
+            'position' => 333,
+            'system' => false,
+            'backend' => ''
+        ]);
+
+        
+        $attribute = $customerSetup->getEavConfig()->getAttribute('customer', 'website')
+            ->addData(['used_in_forms' => [
+                'adminhtml_customer',
+                'adminhtml_checkout',
+                'customer_account_create',
+                'customer_account_edit'
+            ]]);
+        $attribute->save();
+        
+
+        $customerSetup->addAttribute(\Magento\Customer\Model\Customer::ENTITY, 'tell_me_about_your_business', [
+            'type' => 'text',
+            'label' => 'Tell Me About Your Business',
+            'input' => 'textarea',
+            'source' => '',
+            'required' => false,
+            'visible' => true,
+            'position' => 333,
+            'system' => false,
+            'backend' => ''
+        ]);
+
+        
+        $attribute = $customerSetup->getEavConfig()->getAttribute('customer', 'tell_me_about_your_business')
+            ->addData(['used_in_forms' => [
+                'adminhtml_customer',
+                'adminhtml_checkout',
+                'customer_account_create',
+                'customer_account_edit'
+            ]]);
+        $attribute->save();
+    }
+}
